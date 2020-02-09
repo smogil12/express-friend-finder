22adb875919217c1936bea0a03eda3a72bf6d563
Spencer Mogil <smogil12@gmail.com>
2020-02-09 16:26:17 -0500

    link css to html pages

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,27 +2,27 @@
 
 var friends = [
   {
-    name: "Friend 1",
+    name: "Tony Soprano",
     photo: "fill in code",
     scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
   },
   {
-    name: "Friend 2",
+    name: "Christopher Moltisanti",
     photo: "fill in code",
     scores: [2, 5, 4, 1, 2, 3, 3, 2, 3, 3]
   },
   {
-    name: "Friend 3",
+    name: "Paulie Walnuts Gualtieri",
     photo: "fill in code",
     scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
   },
   {
-    name: "Friend 4",
+    name: "Silvio Dante",
     photo: "fill in code",
     scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
   },
   {
-    name: "Friend 5",
+    name: "Salvatore Bonpensiero",
     photo: "fill in code",
     scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 3]
   }
