0ea8183ab6bd8dc4d533c6cdcc8ecf8d29ea27a7
Spencer Mogil <smogil12@gmail.com>
2020-02-09 17:39:08 -0500

    chore: redo numbers for characters

--- app/data/friends.js
+++ app/data/friends.js
@@ -5,31 +5,31 @@ var friends = [
     name: "Tony Soprano",
     photo:
       "https://m.media-amazon.com/images/M/MV5BMTkxODg1MTk5MF5BMl5BanBnXkFtZTcwMDQwNTU4Mg@@._V1_SY1000_CR0,0,661,1000_AL_.jpg",
-    scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
+    scores: [5, 1, 1, 1, 1, 5, 2, 3, 3, 5]
   },
   {
     name: "Christopher Moltisanti",
     photo:
       "https://m.media-amazon.com/images/M/MV5BODU4YTNiMmUtMTM5Mi00MjU0LWJhOGQtNWQ0MzNjZTZmMDJiXkEyXkFqcGdeQXVyNDIyNjA2MTk@._V1_.jpg",
-    scores: [2, 5, 4, 1, 2, 3, 3, 2, 3, 3]
+    scores: [2, 2, 5, 2, 2, 3, 2, 2, 4, 2]
   },
   {
     name: "Paulie Walnuts Gualtieri",
     photo:
       "https://m.media-amazon.com/images/M/MV5BM2E2OTRjOTYtMDAwNi00MmYyLTk3N2MtYjJjODM4NzVmMGFlXkEyXkFqcGdeQXVyNDIyNjA2MTk@._V1_.jpg",
-    scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
+    scores: [3, 1, 4, 3, 5, 4, 4, 4, 4, 2]
   },
   {
     name: "Silvio Dante",
     photo:
       "https://m.media-amazon.com/images/M/MV5BMTc3OTIxNDQ3NV5BMl5BanBnXkFtZTgwMDg0NDIzMjE@._V1_.jpg",
-    scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
+    scores: [3, 1, 2, 5, 3, 2, 5, 5, 1, 2]
   },
   {
     name: "Salvatore Bonpensiero",
     photo:
       "https://m.media-amazon.com/images/M/MV5BMjEzNTA3MDE0N15BMl5BanBnXkFtZTgwNzU0NDIzMjE@._V1_.jpg",
-    scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 3]
+    scores: [2, 5, 3, 4, 3, 2, 2, 1, 1, 2]
   }
 ];
 
