da67f1480914ec1f8b043efbde8a0c9eab9f382b
Spencer Mogil <smogil12@gmail.com>
2020-02-09 16:50:35 -0500

    chore: add img urls to friends.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,27 +3,32 @@
 var friends = [
   {
     name: "Tony Soprano",
-    photo: "fill in code",
+    photo:
+      "https://m.media-amazon.com/images/M/MV5BMTkxODg1MTk5MF5BMl5BanBnXkFtZTcwMDQwNTU4Mg@@._V1_SY1000_CR0,0,661,1000_AL_.jpg",
     scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
   },
   {
-    name: "Christopher Moltisanti",
+    name:
+      "https://m.media-amazon.com/images/M/MV5BODU4YTNiMmUtMTM5Mi00MjU0LWJhOGQtNWQ0MzNjZTZmMDJiXkEyXkFqcGdeQXVyNDIyNjA2MTk@._V1_.jpg",
     photo: "fill in code",
     scores: [2, 5, 4, 1, 2, 3, 3, 2, 3, 3]
   },
   {
     name: "Paulie Walnuts Gualtieri",
-    photo: "fill in code",
+    photo:
+      "https://m.media-amazon.com/images/M/MV5BM2E2OTRjOTYtMDAwNi00MmYyLTk3N2MtYjJjODM4NzVmMGFlXkEyXkFqcGdeQXVyNDIyNjA2MTk@._V1_.jpg",
     scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
   },
   {
     name: "Silvio Dante",
-    photo: "fill in code",
+    photo:
+      "https://m.media-amazon.com/images/M/MV5BMTc3OTIxNDQ3NV5BMl5BanBnXkFtZTgwMDg0NDIzMjE@._V1_.jpg",
     scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
   },
   {
     name: "Salvatore Bonpensiero",
-    photo: "fill in code",
+    photo:
+      "https://m.media-amazon.com/images/M/MV5BMjEzNTA3MDE0N15BMl5BanBnXkFtZTgwNzU0NDIzMjE@._V1_.jpg",
     scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 3]
   }
 ];
