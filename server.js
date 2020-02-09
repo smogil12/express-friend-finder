b9315f273b50696546ddd6a94ae98514ce40fde6
Spencer Mogil <smogil12@gmail.com>
2020-02-09 15:55:56 -0500

    feat: can serve static css on server

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var app = express();
 
 var PORT = process.env.PORT || 8080;
 
+app.use(express.static(__dirname + "/app/public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
