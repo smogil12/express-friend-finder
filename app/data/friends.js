39b0e6e8d2485b3f2935b726affc9da124bdf41b
Spencer Mogil <smogil12@gmail.com>
2020-02-09 15:10:03 -0500

    feat: friends page data exportable

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,1 +1,31 @@
 // save application data here as an array of objects
+
+var friends = [
+  {
+    name: "Friend 1",
+    photo: "fill in code",
+    scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
+  },
+  {
+    name: "Friend 2",
+    photo: "fill in code",
+    scores: [2, 5, 4, 1, 2, 3, 3, 2, 3, 3]
+  },
+  {
+    name: "Friend 3",
+    photo: "fill in code",
+    scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
+  },
+  {
+    name: "Friend 4",
+    photo: "fill in code",
+    scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
+  },
+  {
+    name: "Friend 5",
+    photo: "fill in code",
+    scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 3]
+  }
+];
+
+module.exports = friends;
