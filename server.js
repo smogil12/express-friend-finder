9db67a018a12b1cb32000dddb0e78ac911c1b008
Spencer Mogil <smogil12@gmail.com>
2020-02-09 23:29:27 -0500

    chore: add body parser

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 var express = require("express");
 var path = require("path");
-
+var bodyParser = require("body-parser");
 var app = express();
 
 var PORT = process.env.PORT || 8080;
 
 app.use(express.static(__dirname + "/app/public"));
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
 require("./app/routing/htmlRoutes.js")(app);
 require("./app/routing/apiRoutes.js")(app);
